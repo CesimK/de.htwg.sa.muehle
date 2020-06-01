9e64dd1de584d3f7df3819ad6a7b7b19d45314a4
CesimK <CesimKeskin@aol.com>
2020-06-01 14:27:33 +0200

    added playerToHTML

--- player/src/main/scala/Player.scala
+++ player/src/main/scala/Player.scala
@@ -5,4 +5,6 @@ case class Player(name: String, color:String, placed:Int = 0, stones:Int = 9, mi
 
 
   override def toString:String = "%s (%s)".format(name, color)
+
+  def playerToHTML: String = "<p  style=\"font-family:'Lucida Console', monospace\"> " + toString +"</p>"
 }
