d1bab94225932216f08dd2ba39a73ca205f2ba11
ChristopherGogl <christopher.gogl@web.de>
2020-06-01 13:55:15 +0200

    add basic functions

--- player/src/main/scala/Database.scala
+++ player/src/main/scala/Database.scala
@@ -1,3 +1,13 @@
-class Database {
+package playerManager
 
+class Database {
+  var playerLookup:Map[Int, Player] = Map();
+
+  def addPlayer(id:Int, name:String, color:String) = {
+    playerLookup = playerLookup + (id, new Player(name,color, id=id))
+  }
+
+  def removePlayer(id:Int) = {
+    playerLookup = playerLookup - id
+  }
 }
