d296c473b458d4346df669d1404b8d131dd093e5
ChristopherGogl <christopher.gogl@web.de>
2020-06-01 14:32:56 +0200

    implement functions

--- player/src/main/scala/Database.scala
+++ player/src/main/scala/Database.scala
@@ -1,13 +1,21 @@
 package playerManager
-
+import  scala.collection.mutable.Map
 class Database {
-  var playerLookup:Map[Int, Player] = Map();
+  var playerLookup:scala.collection.mutable.Map[String, Player] = Map();
+  var color = "B"
 
-  def addPlayer(id:Int, name:String, color:String) = {
-    playerLookup = playerLookup + (id, new Player(name,color, id=id))
+  def addPlayer(name:String) = {
+    if (color.equals("W")) color = "B" else color = "W"
+    playerLookup.put(name, new Player(name,color))
   }
 
-  def removePlayer(id:Int) = {
-    playerLookup = playerLookup - id
+  def removePlayer(id:String) = {
+    playerLookup.remove(id)
+  }
+  def enterName(name:String):Player = {
+    playerLookup.get(name) match {
+      case Some(player) => player
+      case None => addPlayer(name).get
+    }
   }
 }
