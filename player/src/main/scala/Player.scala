97b29161863c5f2282583b97f0433abe0200f62a
CesimK <CesimKeskin@aol.com>
2020-06-01 14:06:15 +0200

    removed player id

--- player/src/main/scala/Player.scala
+++ player/src/main/scala/Player.scala
@@ -1,6 +1,6 @@
 package player
 
-case class Player(name: String, color:String, placed:Int = 0, stones:Int = 9, mills:Int = 0, id:Int = -1) {
+case class Player(name: String, color:String, placed:Int = 0, stones:Int = 9, mills:Int = 0) {
   val jump:Boolean = stones <= 3
 
 
